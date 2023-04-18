378a2c3bf2bac57068cd726d786b7343ae566348
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-04-18 15:39:05 -0300

    agrego correccion al archivo js

--- js/lista-de-tareas.js
+++ js/lista-de-tareas.js
@@ -2,5 +2,39 @@
 tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista 
 debe poder ser eliminado con un botón creado para ese fin. 
 */
-let listaDeTareas = []
-let tareas = ''
+// let listaTareas = []
+// let tareas = '',
+
+// 
+// //usar delete  o splice para eliminar una tarea de la lista 
+
+let formulario = document.getElementById('formulario');
+let tareaInput = document.getElementById('tareaInput');
+let listaTareas = document.getElementById('listaTareas');
+let tareas = [];
+
+formulario.addEventListener('submit', agregarTarea);
+
+function agregarTarea(evento) {
+    evento.preventDefault();
+    let tarea = tareaInput.value;
+    if (tarea !== '') {
+        tareas.push(tarea);
+        mostrarTareas();
+        tareaInput.value = '';
+    }
+}
+
+function mostrarTareas() {
+    let contenidoLista = '';
+    for (let i = 0; i < tareas.length; i++) {
+        contenidoLista += `<li>${tareas[i]} <button onclick="eliminarTarea(${i})">Eliminar</button></li>`;
+    }
+    listaTareas.innerHTML = contenidoLista;
+}
+
+function eliminarTarea(indice) {
+    tareas.splice(indice, 1);
+    mostrarTareas();
+}
+// eliminarTarea(` class="bg-dark text-light"`)
